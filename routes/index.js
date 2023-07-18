a700622ec69a7be040283f6b9ed57c19c7bfdd8d
Anastasia Gladkikh <avpspb117@gmail.com>
2023-07-18 17:46:27 +0300

    bug fix5+readme

--- routes/index.js
+++ routes/index.js
@@ -1,15 +1,14 @@
-const router=require('express').Router();
+const router = require("express").Router();
+const NOT_FOUND_CODE = 404;
 
-const userRouter= require('./users');
-const cardRouter= require('./cards');
+const userRouter = require("./users");
+const cardRouter = require("./cards");
 
+router.use("/users", userRouter);
+router.use("/cards", cardRouter);
 
-router.use('/users',userRouter);
-router.use('/cards',cardRouter);
-
-
-router.use((req, res)=>{
-  res.status (404),send({message:`Not found ${req.path}`});
+router.use((req, res) => {
+  res.status(NOT_FOUND_CODE).send({ message: "Страница не найдена" });
 });
 
-module.exports=router;
+module.exports = router;
