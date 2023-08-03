71f98322ac930f4e38162d4c73f14472c0307f3b
Anastasia Gladkikh <avpspb117@gmail.com>
2023-08-03 20:35:56 +0300

    bugfix5

--- validation/idCardValidation.js
+++ validation/idCardValidation.js
@@ -2,7 +2,7 @@ const { celebrate, Joi } = require('celebrate');
 
 const idCardValidation = celebrate({
   params: Joi.object().keys({
-    cardId: Joi.string().alphanum().length(12),
+    cardId: Joi.string().alphanum().length(24),
   }),
 });
 
--- validation/idUserValidation.js
+++ validation/idUserValidation.js
@@ -2,7 +2,7 @@ const { celebrate, Joi } = require('celebrate');
 
 const idUserValidation = celebrate({
   params: Joi.object().keys({
-    userId: Joi.string().alphanum().length(12),
+    userId: Joi.string().alphanum().length(24),
   }),
 });
 
